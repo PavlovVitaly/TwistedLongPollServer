b53d335dfd3273ffec1cc2eac12b28e94221df70
Pavlov Vitaly <pvs.mazilla@gmail.com>
2017-11-15 00:07:02 +0300

    Test Twisted server.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from twisted.internet import reactor, endpoints
 from twisted.web import server, resource
 
+TESTDATA = {'server': 'http://127.0.0.1:8000',
+            'key': 'qwerty',
+            'ts': '0'}
+
 
 class Counter(resource.Resource):
     isLeaf = True
@@ -10,8 +14,8 @@ class Counter(resource.Resource):
         self.numberRequests += 1
         request.setHeader(b"content-type", b"text/plain")
         content = u"I am request #{}\n".format(self.numberRequests)
-        return content.encode("ascii")
-
+        # return content.encode("ascii")
+        return str(TESTDATA).encode("ascii")
 
 endpoints.serverFromString(reactor, "tcp:8080").listen(server.Site(Counter()))
 reactor.run()
