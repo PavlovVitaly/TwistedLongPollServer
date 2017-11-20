fdcdbceb87620f1a6f6f4721a1b1f6f7247bf0c9
Pavlov Vitaly <pvs.mazilla@gmail.com>
2017-11-20 22:44:43 +0300

    Part of transaction in database is done.

--- Database.py
+++ Database.py
@@ -12,42 +12,44 @@ class UsersDatabase(object):
         self.__db_type = db_type
         self.__name_db = name_db
         self.dbpool = adbapi.ConnectionPool(self.__db_type, self.__name_db, check_same_thread=False)
-        # self.__create_db_tables()
-        # self.insert_client(self.__SERVER, self.__SERVER_PASSWORD)
+        self.__create_tables().addCallback(lambda x: self.insert_clients(((self.__SERVER, self.__SERVER_PASSWORD),)))
 
-    def __create_db_tables(self):
-        self.dbpool.runQuery("CREATE TABLE IF NOT EXISTS clients (id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                             "login TEXT NOT NULL, "
-                             "password TEXT NOT NULL)")
-        self.dbpool.runQuery("CREATE TABLE IF NOT EXISTS events_log (id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                             "ts DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, "
-                             "event_description TEXT NOT NULL, "
-                             "client_events INTEGER, "
-                             "FOREIGN KEY(client_events) REFERENCES clients(id)))")
+    def __create_tables(self):
+        return self.dbpool.runInteraction(self.__create_db_tables)
 
-    # def insert_client(self, login, password):
-    #     if not self.__get_client_id(login):
-    #         self.dbpool.runQuery("INSERT INTO clients (login, password) VALUES(?, ?)", (login, password))
-    #         return True
-    #     return False
-    def insert_client(self, login, password):
-        self.dbpool.runQuery("INSERT INTO clients (login, password) VALUES(?, ?)", (login, password))
+    def __create_db_tables(self, transaction):
+        transaction.execute("CREATE TABLE IF NOT EXISTS clients (id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                            "login TEXT NOT NULL, "
+                            "password TEXT NOT NULL, "
+                            "CONSTRAINT login_unique UNIQUE (login))")
+        transaction.execute("CREATE TABLE IF NOT EXISTS events_log (id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            "ts DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, "
+                            "event_description TEXT NOT NULL, "
+                            "client_events INTEGER, "
+                            "FOREIGN KEY(client_events) REFERENCES clients(id))")
+
+    def insert_clients(self, login_password):
+        return self.dbpool.runInteraction(self.__insert_clients, login_password)
+
+    def __insert_clients(self, transaction, login_password):
+        for login, password in login_password:
+            transaction.execute("INSERT INTO clients (login, password) VALUES(?, ?)", (login, password))
 
     def insert_event(self, login, ts, event):
         if self.__get_client_id(login):
             self.dbpool.runQuery("INSERT INTO events_log (ts, event_description, client_events) "
                                  "VALUES(?, ?, (SELECT id FROM clients WHERE login = ?))", (event, ts, login))
 
-    def __get_client_id(self, login):
-        return self.dbpool.runQuery("SELECT id FROM clients WHERE login = ?", (login,))
+    def insert_events(self, ts_event_login):
+        return self.dbpool.runInteraction(self._insert_events, ts_event_login)
 
     def _insert_events(self, transaction, ts_event_login):
         for ts, event, login in ts_event_login:
             transaction.execute("INSERT INTO events_log (ts, event_description, client_events) "
                                 "VALUES(?, ?, (SELECT id FROM clients WHERE login = ?))", (event, ts, login))
 
-    def insert_events(self, ts_event_login):
-        return self.dbpool.runInteraction(self._insert_events, ts_event_login)
+    def __get_client_id(self, login):
+        return self.dbpool.runQuery("SELECT id FROM clients WHERE login = ?", (login,))
 
     def get_password(self, login):
         return self.dbpool.runQuery("SELECT password FROM clients WHERE login = ?", (login,))
@@ -80,13 +82,15 @@ def printError(results):
 db = UsersDatabase(SQLITE3, DATABASE_NAME)
 
 
-# db.insert_client('jd', '123')
-# db.insert_client('ww', 'dsfsdgd')
-# db.insert_client('qw', 'dfsd')
-# db.insert_client('ty', '1reetrt3')
-# results = db.get_clients()
-# results.addCallback(printResults)
-# results.addErrback(printError)
+def insert_clients_in_db():
+    clients_password = (('jd', '123'), ('ww', 'dsfsdgd'), ('qw', 'dfsd'), ('ty', '1reetrt3'))
+    db.insert_clients(clients_password).addErrback(printError)
+
+
+def show_db_content():
+    results = db.get_clients()
+    results.addCallback(printResults)
+    results.addErrback(printError)
 
 
 def end():
@@ -94,6 +98,7 @@ def end():
     db.finish()
     reactor.stop()
 
-
-reactor.callLater(1, end)
+reactor.callLater(5, insert_clients_in_db)
+reactor.callLater(10, show_db_content)
+reactor.callLater(15, end)
 reactor.run()
